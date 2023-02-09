00e44b2b4d318f43411dfdee53e4ec13f0db8e75
James Clarke <james@clarkezone.net>
2023-02-09 03:23:38 +0000

    Tweek telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -21,6 +21,7 @@ end, { desc = '[/] Fuzzily search in current buffer]' })
 -- sudo apt-get install ripgrep
 
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>ps', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
