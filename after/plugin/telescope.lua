89c0cd8765c2d9d7b8f1840a10be02cc052c7562
James Clarke <james@clarkezone.net>
2023-03-21 01:04:25 +0000

    Find in current dir

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -25,3 +25,7 @@ vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc =
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>ps', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>pc', function()
+  require('telescope.builtin').find_files( { cwd = vim.fn.expand('%:p:h') })
+end, { desc = 'fuzzily search in current dir' })
